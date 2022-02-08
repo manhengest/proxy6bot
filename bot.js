e9f1c1af219615016b3827ea9bbebb84da8782da
manhengest <manhengest@gmail.com>
2022-02-08 12:21:35 +0200

    timer changed to 1 sec stop command added

--- bot.js
+++ bot.js
@@ -1,32 +1,33 @@
-const { Telegraf } = require('telegraf')
+const { Telegraf } = require("telegraf")
 const bot = new Telegraf("5209490378:AAHJqn2dXIuLttJmBIyL_GocDmdRp0x1kRU")
 const PROXY6_API_KEY = "3843269ba8-e247985298-f56e06e161"
 const axios = require("axios")
 
 const COUNTRY_CODE = "ua"
 
+let checker
+
 bot.start((ctx) => {
 	ctx.reply("Слежка включена 👀")
-	ctx.reply("Как только прокси будут доступны для покупки - я тебе скажу.")
 
-	const checker = setInterval(async () => {
-		const availableProxys = await axios.get(`https://proxy6.net/api/${PROXY6_API_KEY}/getcount?country=${COUNTRY_CODE}&version=4`)
+	checker = setInterval(async () => {
+		const availableProxy = await axios.get(`https://proxy6.net/api/${PROXY6_API_KEY}/getcount?country=${COUNTRY_CODE}&version=4`)
 
-		if (availableProxys.data.count) {
-			success(availableProxys.data.count)
+		if (availableProxy.data.count) {
+			success(availableProxy.data.count)
 		}
-	}, 30000)
+	}, 1000)
 
 	const success = (count) => {
 		ctx.reply("Ура! Прокси в наличии! Быстрей заходи на https://proxy6.net/order чтобы купить.")
 		ctx.reply(`Количество доступных для покупки прокси: ${count}`)
-		ctx.reply("Слежка остановлена. Обращайся если будет нужна помощь 😉")
-
-		clearInterval(checker)
 	}
 })
 
-
+bot.command("stop", (ctx) => {
+	ctx.reply("Слежка остановлена. Обращайся если будет нужна помощь 😉")
+	clearInterval(checker)
+})
 
 
 
